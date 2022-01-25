e0a917b27a04a6fa8ce61aec2296e2b53ff866d3
Rajnish k <rajnish94k@gmail.com>
2022-01-25 19:07:22 +0530

    filter the list

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
 	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 	const addExpenseHandler = expense => {
-		console.log(expense);
+		// console.log(expense);
 		setExpenses((prevExpenses) => {
 			return[expense, ...prevExpenses];
 		});
